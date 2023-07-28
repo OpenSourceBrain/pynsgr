d61815b1a4a93616af8df71014006295fd4823af
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-07-28 11:50:58 +0100

    chore: format with black

--- pynsgr/pyjavaproperties.py
+++ pynsgr/pyjavaproperties.py
@@ -39,7 +39,7 @@ same key.
 
 """
 
-import sys,os
+import sys, os
 import re
 import time
 import traceback
@@ -49,20 +49,19 @@ PY3 = sys.version_info > (3,)
 
 
 class IllegalArgumentException(Exception):
-
     def __init__(self, lineno, msg):
         self.lineno = lineno
         self.msg = msg
 
     def __str__(self):
-        s='Exception at line number %d => %s' % (self.lineno, self.msg)
+        s = "Exception at line number %d => %s" % (self.lineno, self.msg)
         return s
 
+
 class Properties(object):
-    """ A Python replacement for java.util.Properties """
+    """A Python replacement for java.util.Properties"""
 
     def __init__(self, props=None):
-
         # Note: We don't take a default properties object
         # as argument yet
 
@@ -78,22 +77,22 @@ class Properties(object):
         # dictionary to pristine dictionary
         self._keymap = {}
 
-        self.othercharre = re.compile(r'(?<!\\)(\s*\=)|(?<!\\)(\s*\:)')
-        self.othercharre2 = re.compile(r'(\s*\=)|(\s*\:)')
-        self.bspacere = re.compile(r'\\(?!\s$)')
+        self.othercharre = re.compile(r"(?<!\\)(\s*\=)|(?<!\\)(\s*\:)")
+        self.othercharre2 = re.compile(r"(\s*\=)|(\s*\:)")
+        self.bspacere = re.compile(r"\\(?!\s$)")
 
     def __str__(self):
-        s='{'
-        for key,value in list(self._props.items()):
-            print("%s = %s"%(key,value))
-            s = ''.join((s,key,'=',str(value),', '))
+        s = "{"
+        for key, value in list(self._props.items()):
+            print("%s = %s" % (key, value))
+            s = "".join((s, key, "=", str(value), ", "))
 
-        s=''.join((s[:-2],'}'))
+        s = "".join((s[:-2], "}"))
         return s
 
     def __parse(self, lines):
-        """ Parse a list of lines and create
-        an internal property dictionary """
+        """Parse a list of lines and create
+        an internal property dictionary"""
 
         # Every line in the file must consist of either a comment
         # or a key-value pair. A key-value pair is a line consisting
@@ -127,18 +126,20 @@ class Properties(object):
         # This is a line parser. It parses the
         # contents like by line.
 
-        lineno=0
+        lineno = 0
         i = iter(lines)
 
         for line in i:
             lineno += 1
             line = line.strip()
             # Skip null lines
-            if not line: continue
+            if not line:
+                continue
             # Skip lines which are comments
-            if line[0] in ('#','!'): continue
+            if line[0] in ("#", "!"):
+                continue
             # Some flags
-            escaped=False
+            escaped = False
             # Position of first separation char
             sepidx = -1
             # A flag for performing wspace re check
@@ -151,7 +152,7 @@ class Properties(object):
                 first, last = m.span()
                 start, end = 0, first
                 flag = 1
-                wspacere = re.compile(r'(?<![\\\=\:])(\s)')
+                wspacere = re.compile(r"(?<![\\\=\:])(\s)")
             else:
                 if self.othercharre2.search(line):
                     # Check if either '=' or ':' is present
@@ -161,7 +162,7 @@ class Properties(object):
                     # This means, we need to modify the
                     # wspacere a bit, not to look for
                     # : or = characters.
-                    wspacere = re.compile(r'(?<![\\])(\s)')
+                    wspacere = re.compile(r"(?<![\\])(\s)")
                 start, end = 0, len(line)
 
             m2 = wspacere.search(line, start, end)
@@ -178,12 +179,11 @@ class Properties(object):
                 sepidx = last - 1
                 # print line[sepidx]
 
-
             # If the last character is a backslash
             # it has to be preceded by a space in which
             # case the next line is read as part of the
             # same property
-            while line[-1] == '\\':
+            while line[-1] == "\\":
                 # Read next line
                 nextline = next(i)
                 nextline = nextline.strip()
@@ -193,14 +193,14 @@ class Properties(object):
 
             # Now split to key,value according to separation char
             if sepidx != -1:
-                key, value = line[:sepidx], line[sepidx+1:]
+                key, value = line[:sepidx], line[sepidx + 1 :]
             else:
-                key,value = line,''
+                key, value = line, ""
             self._keyorder.append(key)
             self.processPair(key, value)
 
     def processPair(self, key, value):
-        """ Process a (key, value) pair """
+        """Process a (key, value) pair"""
 
         oldkey = key
         oldvalue = value
@@ -212,15 +212,15 @@ class Properties(object):
         strippable = False
         lastpart = keyparts[-1]
 
-        if lastpart.find('\\ ') != -1:
-            keyparts[-1] = lastpart.replace('\\','')
+        if lastpart.find("\\ ") != -1:
+            keyparts[-1] = lastpart.replace("\\", "")
 
         # If no backspace is found at the end, but empty
         # space is found, strip it
-        elif lastpart and lastpart[-1] == ' ':
+        elif lastpart and lastpart[-1] == " ":
             strippable = True
 
-        key = ''.join(keyparts)
+        key = "".join(keyparts)
         if strippable:
             key = key.strip()
             oldkey = oldkey.strip()
@@ -253,32 +253,32 @@ class Properties(object):
             self._keyorder.append(key)
 
     def escape(self, value):
-
         # Java escapes the '=' and ':' in the value
         # string with backslashes in the store method.
         # So let us do the same.
-        newvalue = value.replace(':','\:')
-        newvalue = newvalue.replace('=','\=')
+        newvalue = value.replace(":", "\:")
+        newvalue = newvalue.replace("=", "\=")
 
         return newvalue
 
     def unescape(self, value):
-
         # Reverse of escape
-        newvalue = value.replace('\:',':')
-        newvalue = newvalue.replace('\=','=')
+        newvalue = value.replace("\:", ":")
+        newvalue = newvalue.replace("\=", "=")
 
         return newvalue
 
     def load(self, stream):
-        """ Load properties from an open file stream """
+        """Load properties from an open file stream"""
 
         # For the time being only accept file input streams
-        if (PY3 and not isinstance(stream, IOBase)) or (not PY3 and not isinstance(stream, file)):
-            raise TypeError('Argument should be a file object!')
+        if (PY3 and not isinstance(stream, IOBase)) or (
+            not PY3 and not isinstance(stream, file)
+        ):
+            raise TypeError("Argument should be a file object!")
         # Check for the opened mode
-        if stream.mode != 'r':
-            raise ValueError('Stream should be opened in read-only mode!')
+        if stream.mode != "r":
+            raise ValueError("Stream should be opened in read-only mode!")
 
         try:
             lines = stream.readlines()
@@ -296,63 +296,61 @@ class Properties(object):
             return self._props.get(key)[0]
         return None
 
-
     def setProperty(self, key, value):
-        """ Set the property for the given key """
+        """Set the property for the given key"""
 
         if type(key) is str and type(value) is str:
             self.processPair(key, value)
         else:
-            raise TypeError('both key and value should be strings!')
+            raise TypeError("both key and value should be strings!")
 
     def propertyNames(self):
-        """ Return an iterator over all the keys of the property
-        dictionary, i.e the names of the properties """
+        """Return an iterator over all the keys of the property
+        dictionary, i.e the names of the properties"""
 
         return list(self._props.keys())
 
     def list(self, out=sys.stdout):
-        """ Prints a listing of the properties to the
-        stream 'out' which defaults to the standard output """
+        """Prints a listing of the properties to the
+        stream 'out' which defaults to the standard output"""
 
-        out.write('-- listing properties --\n')
-        for key,value in list(self._props.items()):
+        out.write("-- listing properties --\n")
+        for key, value in list(self._props.items()):
             # out.write(''.join((key,'='value,'\n')))
             if not value or not len(value):
-                out.write(''.join((key,'=','\n')))
+                out.write("".join((key, "=", "\n")))
             else:
                 for v in value:
-                    out.write(''.join((key,'=', v, '\n')))
-                
+                    out.write("".join((key, "=", v, "\n")))
 
     def store(self, out, header=""):
-        """ Write the properties list to the stream 'out' along
-        with the optional 'header' """
+        """Write the properties list to the stream 'out' along
+        with the optional 'header'"""
 
-        if out.mode[0] != 'w':
-            raise ValueError('Steam should be opened in write mode!')
+        if out.mode[0] != "w":
+            raise ValueError("Steam should be opened in write mode!")
 
         try:
-            out.write(''.join(('#',header,'\n')))
+            out.write("".join(("#", header, "\n")))
             # Write timestamp
-            tstamp = time.strftime('%a %b %d %H:%M:%S %Z %Y', time.localtime())
-            out.write(''.join(('#',tstamp,'\n')))
+            tstamp = time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime())
+            out.write("".join(("#", tstamp, "\n")))
             # Write properties from the pristine dictionary
             for prop in self._keyorder:
                 if prop in self._origprops:
                     val = self._origprops[prop]
-                    out.write(''.join((prop,'=',self.escape(val),'\n')))
+                    out.write("".join((prop, "=", self.escape(val), "\n")))
 
             out.close()
         except IOError as e:
             raise
 
-    # Terri: returns the internal dictionary converted such each value is a string, the first one in the list from the raw dict 
+    # Terri: returns the internal dictionary converted such each value is a string, the first one in the list from the raw dict
     def getPropertyDict(self):
         retval = {}
         for key in self._props:
             retval[key] = self._props[key][0]
-        return retval 
+        return retval
 
     # Terri: returns the internal dictionary object, where each value is a list.
     def getRawPropertyDict(self):
@@ -364,41 +362,41 @@ class Properties(object):
         retval = []
         for key in self._props:
             for value in self._props[key]:
-                retval.append( (key, value) )
-        return retval 
+                retval.append((key, value))
+        return retval
 
     def __getitem__(self, name):
-        """ To support direct dictionary like access """
+        """To support direct dictionary like access"""
 
         return self.getProperty(name)
 
     def __setitem__(self, name, value):
-        """ To support direct dictionary like access """
+        """To support direct dictionary like access"""
 
         self.setProperty(name, value)
 
     # Terri - I modified this to make it work so that you can access properties as properties.name
     # instead of just properties['name'].
     def __getattr__(self, name):
-        """ For attributes not found in self, redirect to the properties dictionary """
+        """For attributes not found in self, redirect to the properties dictionary"""
         try:
             return self.__dict__[name]
         except KeyError:
             r = self._props.get(name)
             if r:
                 return self._props[name][0]
-            return None 
-                
+            return None
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     p = Properties()
-    p.load(open('/Users/padraig/pycipres.conf'))
+    p.load(open("/Users/padraig/pycipres.conf"))
     p.list()
     print(p)
     print(list(p.items()))
-    print(p['name3'])
-    p['name3'] = 'changed = value'
-    print(p['name3'])
-    p['new key'] = 'new value'
-    p.store(open('test2.properties','w'))
+    print(p["name3"])
+    p["name3"] = "changed = value"
+    print(p["name3"])
+    p["new key"] = "new value"
+    p.store(open("test2.properties", "w"))
     print(p.name3)
