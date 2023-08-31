7740ff0f8e5d97bcc379ff6c47f402f88e63e8fb
pgleeson <p.gleeson@gmail.com>
2023-08-31 18:26:07 +0100

    Testing on toolId=NEURON_EXPANSE, as that's what we use for OSBv1

--- example/example_dir/example.py
+++ example/example_dir/example.py
@@ -10,9 +10,13 @@ Copyright 2023 OSB contributors
 import sys
 import pkg_resources
 
+print("Running example script to list Python packages...")
+
 with open("output.txt", "w") as f:
-    print(f"Python version: {sys.version}", file=f)
-    print("Installed packages:", file=f)
+    print(f"Python version is: {sys.version}", file=f)
+    print("Installed packages on NSG:", file=f)
     dists = [str(d).replace(" ", "==") for d in pkg_resources.working_set]
     for i in dists:
         print(i, file=f)
+
+print("Done!")
