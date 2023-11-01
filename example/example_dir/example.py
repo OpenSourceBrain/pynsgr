5ed2997eaefc30aaf5d22c6d33951e0404b684fe
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-11-01 18:25:54 +0000

    debug: netpyne

--- example/example_dir/example.py
+++ example/example_dir/example.py
@@ -7,16 +7,21 @@ Copyright 2023 OSB contributors
 """
 
 
+from netpyne import specs
 import sys
 import pkg_resources
 
-print("Running example script to list Python packages...")
+if __name__ == "__main__":
+    print("Running example script to list Python packages...")
 
-with open("output.txt", "w") as f:
-    print(f"Python version is: {sys.version}", file=f)
-    print("Installed packages on NSG:", file=f)
-    dists = [str(d).replace(" ", "==") for d in pkg_resources.working_set]
-    for i in dists:
-        print(i, file=f)
+    with open("output.txt", "w") as f:
+        print(f"Python version is: {sys.version}", file=f)
+        print("Installed packages on NSG:", file=f)
+        dists = [str(d).replace(" ", "==") for d in pkg_resources.working_set]
+        for i in dists:
+            print(i, file=f)
 
-print("Done!")
+    print(f"Args are: {sys.argv}")
+    if sys.argv.count("-nogui") > 0:
+        print("nogui found")
+    print("Done!")
