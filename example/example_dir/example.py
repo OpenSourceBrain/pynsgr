0fb52603f2aeec0e8c14664f509a237f9616812f
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-11-06 09:42:16 +0000

    ci(example): update to test pyneuroml/netpyne imports
    to ensure that matplotlib is not also imported

--- example/example_dir/example.py
+++ example/example_dir/example.py
@@ -10,6 +10,7 @@ Copyright 2023 OSB contributors
 from netpyne import specs
 from netpyne import sim
 from netpyne import __version__ as version
+from pyneuroml import pynml
 
 import sys
 import pkg_resources
@@ -21,14 +22,19 @@ if __name__ == "__main__":
         print(f"Python version is: {sys.version}", file=f)
         print("Installed packages on NSG:", file=f)
         dists = [str(d).replace(" ", "==") for d in pkg_resources.working_set]
-        for i in dists:
-            print(i, file=f)
 
+    # also check if netpyne/pyneuroml etc. cause importing matplotlib: we can't
+    # use matplotlib on NSG because the different mpi processes cause the same
+    # cache file to be read, which causes crashes
+    print("Checking if matplotlib loaded...")
+    print(f"Args are: {sys.argv}")
+    if sys.argv.count("-nogui") > 0:
+        print("nogui option found")
+
+    for i in dists:
+        print(i, file=f)
     for k in sys.modules.keys():
         if "matplotlib" in k:
             print(f"matplotlib still loaded: {k}")
 
-    print(f"Args are: {sys.argv}")
-    if sys.argv.count("-nogui") > 0:
-        print("nogui found")
     print("Done!")
